daccdb086bf70445c907a90a1fc0f34be105b06f
bree <me@bree.dev>
2025-03-16 23:58:14 -0400

    add additional contextual protoflux actions

--- ResoniteBreeTweaks/Patches/ProtoFluxTool_GenerateMenuItems_Patch.cs
+++ ResoniteBreeTweaks/Patches/ProtoFluxTool_GenerateMenuItems_Patch.cs
@@ -9,6 +9,8 @@ using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Users;
 using System.Runtime.CompilerServices;
 using System.Collections.Generic;
 using System.Linq;
+using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes.FrooxEngine.Transform;
+using FrooxEngine.ProtoFlux.Runtimes.Execution.Nodes;
 
 namespace BreeTweaks.Patches;
 
@@ -25,7 +27,7 @@ internal static class ProtoFluxTool_GenerateMenuItems_Patch
     internal static bool Prefix(ProtoFluxTool __instance, SyncRef<ProtoFluxElementProxy> ____currentProxy)
     {
         var elementProxy = ____currentProxy.Target;
-        var items = MenuItems(__instance, elementProxy).Take(10).ToArray();
+        var items = MenuItems(__instance).Take(10).ToArray();
 
         if (items.Length != 0)
         {
@@ -39,22 +41,64 @@ internal static class ProtoFluxTool_GenerateMenuItems_Patch
             {
                 var menu = await __instance.LocalUser.OpenContextMenu(__instance, __instance.Slot);
 
-                if (elementProxy is ProtoFluxInputProxy proxy)
+                switch (elementProxy)
                 {
-                    foreach (var item in items)
-                    {
-                        var label = item.node.Name.AsLocaleKey();
-                        var menuItem = menu.AddItem(in label, (Uri?)null, typeof(User).GetTypeColor());
-                        menuItem.Button.LocalPressed += (button, data) =>
+                    case ProtoFluxInputProxy inputProxy:
                         {
-                            __instance.SpawnNode(item.node, n =>
+                            foreach (var item in items)
                             {
-                                proxy.Node.Target.TryConnectInput(proxy.NodeInput.Target, n.GetOutput(0), allowExplicitCast: false, undoable: true);
-                                __instance.LocalUser.CloseContextMenu(__instance);
-                                CleanupDraggedWire(__instance);
-                            });
-                        };
-                    }
+                                var label = item.node.Name.AsLocaleKey();
+                                var menuItem = menu.AddItem(in label, (Uri?)null, inputProxy.InputType.Value.GetTypeColor());
+                                menuItem.Button.LocalPressed += (button, data) =>
+                                {
+                                    __instance.SpawnNode(item.node, n =>
+                                    {
+                                        inputProxy.Node.Target.TryConnectInput(inputProxy.NodeInput.Target, n.GetOutput(0), allowExplicitCast: false, undoable: true);
+                                        __instance.LocalUser.CloseContextMenu(__instance);
+                                        CleanupDraggedWire(__instance);
+                                    });
+                                };
+                            }
+                            break;
+                        }
+                    case ProtoFluxOutputProxy outputProxy:
+                        {
+                            foreach (var item in items)
+                            {
+                                var label = item.node.Name.AsLocaleKey();
+                                var menuItem = menu.AddItem(in label, (Uri?)null, outputProxy.OutputType.Value.GetTypeColor());
+                                menuItem.Button.LocalPressed += (button, data) =>
+                                {
+                                    __instance.SpawnNode(item.node, n =>
+                                    {
+                                        n.TryConnectInput(n.GetInput(0), outputProxy.NodeOutput.Target, allowExplicitCast: false, undoable: true);
+                                        __instance.LocalUser.CloseContextMenu(__instance);
+                                        CleanupDraggedWire(__instance);
+                                    });
+                                };
+                            }
+                            break;
+                        }
+                    case ProtoFluxImpulseProxy impulseProxy:
+                        {
+                            foreach (var item in items)
+                            {
+                                var label = item.node.Name.AsLocaleKey();
+                                var menuItem = menu.AddItem(in label, (Uri?)null, impulseProxy.ElementContentType.GetTypeColor());
+                                menuItem.Button.LocalPressed += (button, data) =>
+                                {
+                                    __instance.SpawnNode(item.node, n =>
+                                    {
+                                        n.TryConnectImpulse(impulseProxy.NodeImpulse.Target, n.GetOperation(0), undoable: true);
+                                        __instance.LocalUser.CloseContextMenu(__instance);
+                                        CleanupDraggedWire(__instance);
+                                    });
+                                };
+                            }
+                            break;
+                        }
+                    default:
+                        throw new Exception("found items for unsupported protoflux contextual action type");
                 }
             });
 
@@ -64,16 +108,41 @@ internal static class ProtoFluxTool_GenerateMenuItems_Patch
         return true;
     }
 
-    internal static IEnumerable<MenuItem> MenuItems(ProtoFluxTool __instance, ProtoFluxElementProxy target)
+    internal static IEnumerable<MenuItem> MenuItems(ProtoFluxTool __instance)
     {
-        if (target is ProtoFluxInputProxy proxy)
+        var _currentProxy = Traverse.Create(__instance).Field("_currentProxy").GetValue<SyncRef<ProtoFluxElementProxy>>();
+        var target = _currentProxy?.Target;
+
+        if (target is ProtoFluxInputProxy inputProxy)
         {
-            if (proxy.InputType.Value == typeof(User))
+            if (inputProxy.InputType.Value == typeof(User))
             {
                 yield return new MenuItem(typeof(LocalUser));
                 yield return new MenuItem(typeof(HostUser));
                 yield return new MenuItem(typeof(UserFromUsername));
             }
+        }
+
+        if (target is ProtoFluxOutputProxy outputProxy)
+        {
+            if (outputProxy.OutputType.Value == typeof(Slot))
+            {
+                yield return new MenuItem(typeof(GlobalTransform));
+                yield return new MenuItem(typeof(GetForward));
+            }
+        }
+
+        if (target is ProtoFluxImpulseProxy)
+        {
+            yield return new MenuItem(typeof(For));
+            yield return new MenuItem(typeof(If));
+            yield return new MenuItem(typeof(ValueWrite<dummy>));
+            yield return new MenuItem(typeof(Sequence));
+        }
+
+        if (target is ProtoFluxOperationProxy)
+        {
+
         }
     }
 
